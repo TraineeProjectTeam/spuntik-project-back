b1724ad6153b7496014021c68fdebb596cee0889
Veronika <veronika.korolkova99@gmail.com>
2024-06-22 01:34:42 +0700

    Сhore: Adding headers to solve the cors problem. Adding necessary libraries.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const config = new DocumentBuilder()
   .setTitle('Sputnik Project')
   .setDescription('Documentation REST API')
@@ -13,6 +14,11 @@ async function bootstrap() {
   .build()
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document)
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,PUT,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 bootstrap();
