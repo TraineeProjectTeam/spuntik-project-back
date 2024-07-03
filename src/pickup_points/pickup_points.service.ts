f4b43b3d6502035640b177246e0004409db1606f
Andrey <dimitriev01@list.ru>
2024-07-03 22:15:47 +0700

    chore: name of model in service pickup point

--- src/pickup_points/pickup_points.service.ts
+++ src/pickup_points/pickup_points.service.ts
@@ -5,14 +5,14 @@ import { PickupPoint } from './pickup_points.schema';
 
 @Injectable()
 export class PickupPointsService {
-  constructor(@InjectModel('PickupPoint') private readonly customerModel: Model<PickupPoint>) {}
+  constructor(@InjectModel('PickupPoint') private readonly pickupPointsModel: Model<PickupPoint>) {}
 
   async findAll(): Promise<PickupPoint[]> {
-    return await this.customerModel.find().exec();
+    return await this.pickupPointsModel.find().exec();
   }
 
   async findOne(id: string): Promise<PickupPoint> {
-    const result = await this.customerModel.findById(id).exec();
+    const result = await this.pickupPointsModel.findById(id).exec();
     if (!result) {
       throw Error(`Точка выдачи с id ${id} не найдена`);
     }
